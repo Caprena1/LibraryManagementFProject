6b437c2b371f893420cc49c1a8b777435bbbfe65
Caprena Richie <robcaprena@gmail.com>
2021-11-09 22:23:04 -0500

    Add project files.

---
+++ LibraryManagementFProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibraryManagementFProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
